a63c093b2cc5df08122c7cde675a15ce2e2c05f6
DbStudioProducers <yggdrasilagencia@gmail.com>
2026-06-12 13:59:16 -0300

    Create main.js

---
+++ electron/main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280, height: 800,
+    backgroundColor: '#111',
+    webPreferences: { nodeIntegration: true, contextIsolation: false }
+  });
+  win.loadFile(path.join(__dirname, '../dist/index.html'));
+  win.setMenuBarVisibility(false);
+}
+app.whenReady().then(createWindow);
